425e82b1ae872737ffdba03d3ec8bb4d044a7518
vcatafesta <vcatafesta@gmail.com>
2020-08-10 09:55:05 -0400

    upgrade

--- str.c
+++ str.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-    int targetStrLen = 10;           // Target output length  
-    const char *myString="Monkey";   // String for output 
+    int targetStrLen = 10;           // Target output length
+    const char *myString="Monkey";   // String for output
     const char *padding="#####################################################";
 
     int padLen = targetStrLen - strlen(myString); // Calc Padding length
     if(padLen < 0) padLen = 0;    // Avoid negative length
 
-    printf("[%*.*s%s]\n", padLen, padLen, padding, myString);  // LEFT Padding 
-    printf("[%s%*.*s]\n", myString, padLen, padLen, padding);  // RIGHT Padding 
+    printf("[%*.*s%s]\n", padLen, padLen, padding, myString);  // LEFT Padding
+    printf("[%s%*.*s]\n", myString, padLen, padLen, padding);  // RIGHT Padding
     return 0;
 }
