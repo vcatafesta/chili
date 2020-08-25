1e25453fec40d82d59e7b325623185da34c9d780
vcatafesta <vcatafesta@gmail.com>
2020-08-25 18:42:36 -0400

    upgrade

---
+++ rust/hello.rs
@@ -0,0 +1,10 @@
+fn main()
+{
+   let mut idade = "Eu";
+   let mut nome  = "Tharic";
+   println!("Hello, {}, você tem {} anos", idade, nome);
+   idade = str(53);
+   nome = "Vilmar";
+   println!("Hello, {}, você tem {} anos", nome, idade);
+} 
+
