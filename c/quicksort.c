003e4c3e0524d3204319969896eca7947e8b90fa
vcatafesta <vcatafesta@gmail.com>
2020-11-17 10:05:49 -0400

    upgrade

--- c/quicksort.c
+++ c/quicksort.c
@@ -1,34 +1,40 @@
-#include <iostream>
 #include <stdio.h>
-using namespace std;
+#include <stdlib.h>
 
-int LenArray(char *a[])
+#define sizeof_array(ARRAY) (sizeof(ARRAY)/sizeof(ARRAY[0]))
+#define arraylenght(ARRAY) (sizeof(ARRAY)/sizeof(int*))
+#define COUNTOF(x) (sizeof(x)/sizeof(*x))
+
+int LenArray( char *a[])
 {
    int nlen = 0;
-   //while(*(a+nlen)){
    while( a[nlen] ){
      nlen++;
    }
 	return nlen;
 }
 
-void print(int a[]) {
-	int i=0;
-	while( a[i++] ) {
-		cout << a[i] << "-";
+int printarray(int *a)
+{
+	int nlen = sizeof(a) / sizeof(int);
+	printf("%02d------------\n", nlen);
+	for(int i = 0; i <= nlen; i++){
+		printf("%02d\n", a[i]);
 	}
-	cout << endl;
 }
 
-int partition(int a[], int p, int r) {
+int partition(int a[], int p, int r)
+{
+	int ntemp;
 	int x = a[r];
-	int j = p - 1;
+	int j = (p - 1);
+
 	for (int i = p; i < r; i++) {
 		if (x <= a[i]) {
 			j = j + 1;
-			int temp = a[j];
+			ntemp = a[j];
 			a[j] = a[i];
-			a[i] = temp;
+			a[i] = ntemp;
 		}
 	}
 	a[r] = a[j + 1];
@@ -36,18 +42,23 @@ int partition(int a[], int p, int r) {
 	return (j + 1);
 }
 
-void quickSort(int a[], int p, int r) {
+void quickSort(int a[], int p, int r)
+{
 	if (p < r) {
-	int q = partition(a, p, r);
-	quickSort(a, p, q - 1);
-	quickSort(a, q + 1, r);
+		int q = partition(a, p, r);
+		quickSort(a, p, q - 1);
+		quickSort(a, q + 1, r);
 	}
 }
 
-int main() {
-	int a[] = { 1, 9, 0, 5, 6, 7, 8, 2, 4, 3 };
+int main()
+{
+	int a[] = { 1, 9, 3, 5, 6, 7, 8, 2, 4, 0};
+	printf("Original:\n");
+	printarray(a);
 	quickSort(a, 0, 9);
-	print(a);
+	printf("Reordenado:\n");
+	printarray(a);
 	return 0;
 
 }
