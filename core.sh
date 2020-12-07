110f43b33b5f1d0a91d65a05fd99edbad5e2c5de
vcatafesta <vcatafesta@gmail.com>
2020-12-07 01:29:47 -0400

    upgrade

--- core.sh
+++ core.sh
@@ -622,6 +622,29 @@ which2()
 	#chown -v root:root /usr/bin/which
 }
 
+function size_to_human(){
+	awk -v size="$1" '
+	BEGIN {
+		suffix[1] = "B"
+		suffix[2] = "KiB"
+		suffix[3] = "MiB"
+		suffix[4] = "GiB"
+		suffix[5] = "TiB"
+		suffix[6] = "PiB"
+		suffix[7] = "EiB"
+		count = 1
+
+		while (size > 1024) {
+			size /= 1024
+			count++
+		}
+
+		sizestr = sprintf("%.2f", size)
+		sub(/\.?0+$/, "", sizestr)
+		printf("%s %s", sizestr, suffix[count])
+	}'
+}
+
 function ex()
 {
 	if [ -f $1 ] ; then
