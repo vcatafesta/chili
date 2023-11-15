c8c0c8d8b0dd9fa90ade8ff7f5c28859246713db
vcatafesta <vcatafesta@gmail.com>
2023-11-14 21:03:37 -0400

    ter 14 nov 2023 21:03:37 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- sorteio.sh
+++ sorteio.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-nomes=(Vilmar Bruno Celso Washington Filipi Bagatini Mauricio Alcides Leandro Gleiton Ama Ivan  )
+nomes=('Vilmar' 'Bruno' 'Celso' 'Washington' 'Filipi' 'Bagatini' 'Mauricio' 'Alcides' 'Leandro' 'Gleiton' 'Ama' 'Ivan')
 if [ "${#nomes[@]}" -lt 2 ]; then
-	echo "É necessário ter pelo menos duas pessoas para o sorteio."
+	echo "É necessário pelo menos duas pessoas para o sorteio."
 	exit 1
 fi
-num_pessoas=${#nomes[@]}
-indice_sorteado=$((RANDOM % num_pessoas))
-pessoa_sorteada=${nomes[$indice_sorteado]}
-echo "A pessoa sorteada é: $pessoa_sorteada"
+echo "A pessoa sorteada é: ${nomes[$((RANDOM % ${#nomes[@]}))]}"
