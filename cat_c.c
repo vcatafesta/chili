7582997d21d4395c9a4bc352c31c333ef530cae0
arcolinuxz <arcolinuxinfo@gmail.com>
2024-04-16 14:53:30 -0400

    ter 16 abr 2024 14:53:30 -04 Vilmar Catafesta (vcatafesta@gmail.com)

---
+++ cat_c.c
@@ -0,0 +1,63 @@
+// cat_c.c, Copyright (c) 2023 Vilmar Catafesta <vcatafesta@gmail.com>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#define BLACK        "\033[30m"
+#define RED          "\033[31m"
+#define GREEN        "\033[32m"
+#define YELLOW       "\033[33m"
+#define BLUE         "\033[34m"
+#define MAGENTA      "\033[35m"
+#define CYAN         "\033[36m"
+#define WHITE        "\033[37m"
+#define GRAY         "\033[90m"
+#define LIGHTWHITE   "\033[97m"
+#define LIGHTGRAY    "\033[37m"
+#define LIGHTRED     "\033[91m"
+#define LIGHTGREEN   "\033[92m"
+#define LIGHTYELLOW  "\033[93m"
+#define LIGHTBLUE    "\033[94m"
+#define LIGHTMAGENTA "\033[95m"
+#define LIGHTCYAN    "\033[96m"
+#define RESET        "\033[0m"
+#define BOLD         "\033[1m"
+#define FAINT        "\033[2m"
+#define ITALIC       "\033[3m"
+#define UNDERLINE    "\033[4m"
+#define BLINK        "\033[5m"
+#define INVERTED     "\033[7m"
+#define HIDDEN       "\033[8m"
+
+int main(int argc, char **argv) {
+   printf("%scat_c.c, Copyright (c) 2023 Vilmar Catafesta <vcatafesta@gmail.com>%s\n\n", RED, RESET);
+   printf("%sHello World\n%s", GREEN, RESET);
+
+    FILE *arquivo;
+    char linha[1000]; // Tamanho arbitrário para a linha
+
+    // Abre o arquivo para leitura
+    arquivo = fopen("arquivo.txt", "r");
+
+    // Verifica se o arquivo foi aberto corretamente
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    // Lê e imprime cada linha do arquivo
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        printf("%s", linha);
+    }
+
+    // Fecha o arquivo
+    fclose(arquivo);
+
+	return EXIT_SUCCESS;
+}
