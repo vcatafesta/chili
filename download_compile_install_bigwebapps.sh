00e89bd215cf732b86900d41d543c3b3d51beb6d
vcatafesta <vcatafesta@gmail.com>
2024-07-13 11:41:09 -0400

    sáb 13 jul 2024 11:41:09 -04 Vilmar Catafesta (vcatafesta@gmail.com)

---
+++ download_compile_install_bigwebapps.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash disable=SC1091,SC2039,SC2166
+#
+#  download_compile_install_bigwebapps.sh
+#  Created: 2024/07/08 - 14:35
+#  Altered: 2024/07/13 - 11:26
+#
+#  Copyright (c) 2024-2024, Vilmar Catafesta <vcatafesta@gmail.com>
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AS IS'' AND ANY EXPRESS OR
+#  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+#  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+#  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+#  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+#  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+##############################################################################
+#export LANGUAGE=pt_BR
+export TEXTDOMAINDIR=/usr/share/locale
+export TEXTDOMAIN=chili-makebash
+
+#debug
+export PS4='[1m[38;5;196mbash[1m[32m[makebash][35m[15](B[m '
+#set -x
+#set -e
+shopt -s extglob
+
+#system
+readonly APP="bash"
+readonly _VERSION_='1.0.0-20240713 - 11:26'
+readonly distro=voidlinux
+readonly DEPENDENCIES=(grep)
+
+mkdir -p /tmp/biglinux-webapps
+cd /tmp/biglinux-webapps || exit 1
+rm PKGBUILD
+wget https://raw.githubusercontent.com/vcatafesta/biglinux-webapps/main/pkgbuild/PKGBUILD
+sed -i 's|url="https://github.com/biglinux/biglinux-webapps"|url="https://github.com/vcatafesta/biglinux-webapps"|'g PKGBUILD
+makepkg -fsi
+
+mkdir -p /tmp/bigcontrolcenter-base
+cd /tmp/bigcontrolcenter-base || exit 1
+rm PKGBUILD
+wget https://raw.githubusercontent.com/vcatafesta/bigcontrolcenter-base/main/pkgbuild/PKGBUILD
+sed -i 's|url="https://github.com/biglinux/$pkgname"|url="https://github.com/vcatafesta/$pkgname"|'g PKGBUILD
+makepkg -fsi
+
+
---
+++ install-bigwebapps.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+#sh <(curl -s -L https://raw.githubusercontent.com/voidlinux-br/void-installer/master/install.sh)
+#sh <(wget -q -O - https://raw.githubusercontent.com/voidlinux-br/void-installer/master/install.sh)
+#source <(curl -s -L https://raw.githubusercontent.com/voidlinux-br/void-installer/master/install.sh)
+#source <(wget -q -O - https://raw.githubusercontent.com/voidlinux-br/void-installer/master/install.sh)
+
+#  install-bigwebapps.sh
+#  Created: 2024/07/13
+#  Altered: 2024/07/13
+#
+#  Copyright (c) 2023-2024, Vilmar Catafesta <vcatafesta@gmail.com>
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+#  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+#  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+#  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+#  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+#  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+{
+	oops() {
+		echo "$0:" "$@" >&2
+		exit 1
+	}
+
+	umask 0022
+	url="https://raw.githubusercontent.com/voidlinux-br/void-installer/master"
+	declare -a files_bin=('download_compile_install_bigwebapps.sh')
+	tmpDir=~/bigwebapps-install
+
+	[[ ! -d "$tmpDir" ]] && { mkdir -p "$tmpDir" || oops "Unable to create temporary directory to download files"; }
+
+	require_util() {
+		command -v "$1" >/dev/null 2>&1 || oops "you do not have '$1' installed, which is needed to $2"
+	}
+
+	#require_util tar "descompatar o tarball"
+
+	if command -v curl >/dev/null 2>&1; then
+		cmdfetch() { curl --silent --continue-at - --insecure -L "$1" -o "$2"; }
+	elif command -v wget >/dev/null 2>&1; then
+		cmdfetch() { wget --quiet -c "$1" -O "$2"; }
+	else
+		require_util curl "downloader"
+		require_util wget "downloader"
+	fi
+
+	for f in "${files_bin[@]}"; do
+		echo "Downloading $f to '$tmpDir'..."
+		cmdfetch "$url/$f" "$tmpDir/$f" || oops "download failure '$url/$f'"
+	done
+
+	for file in "${files_bin[@]}"; do
+		sudo chmod +x $tmpDir/$file
+		sudo cp -rfv $tmpDir/$file /usr/bin/
+	done
+
+	ls -la --color=auto $tmpDir
+
+	echo
+	echo "digite:"
+	echo "	sudo download_compile_install_bigwebapps.sh"
+	echo "ou entre em: $tmpDir e digite:"
+	echo "	sudo ./download_compile_install_bigwebapps.sh"
+}
+
