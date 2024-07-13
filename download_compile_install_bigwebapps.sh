d3b13161a541ed0cb2a36678edc0ce733847e9c7
vcatafesta <vcatafesta@gmail.com>
2024-07-13 12:37:08 -0400

    sáb 13 jul 2024 12:37:08 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- download_compile_install_bigwebapps.sh
+++ download_compile_install_bigwebapps.sh
@@ -30,12 +30,12 @@
 ##############################################################################
 #export LANGUAGE=pt_BR
 export TEXTDOMAINDIR=/usr/share/locale
-export TEXTDOMAIN=chili-makebash
+export TEXTDOMAIN=download_compile_install_bigwebapps.sh
 
 #debug
-export PS4='[1m[38;5;196mbash[1m[32m[makebash][35m[15](B[m '
+export PS4='[1m[38;5;196mbash[1m[32m[download_compile_install_bigwebapps.sh][35m[15](B[m '
 #set -x
-#set -e
+set -e
 shopt -s extglob
 
 #system
@@ -44,18 +44,22 @@ readonly _VERSION_='1.0.0-20240713 - 11:26'
 readonly distro=voidlinux
 readonly DEPENDENCIES=(grep)
 
-mkdir -p /tmp/biglinux-webapps
-cd /tmp/biglinux-webapps || exit 1
-rm PKGBUILD
-wget https://raw.githubusercontent.com/vcatafesta/biglinux-webapps/main/pkgbuild/PKGBUILD
-sed -i 's|url="https://github.com/biglinux/biglinux-webapps"|url="https://github.com/vcatafesta/biglinux-webapps"|'g PKGBUILD
-makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm
+if mkdir -p /tmp/bigcontrolcenter-base-install; then
+	cd /tmp/bigcontrolcenter-base-install || exit 1
+	[[ -e PKGBUILD ]] && rm PKGBUILD
+	if wget https://raw.githubusercontent.com/vcatafesta/bigcontrolcenter-base/main/pkgbuild/PKGBUILD; then
+		sed -i 's|url="https://github.com/biglinux/$pkgname"|url="https://github.com/vcatafesta/$pkgname"|'g PKGBUILD
+		makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm || exit 1
+	fi
+fi
 
-mkdir -p /tmp/bigcontrolcenter-base
-cd /tmp/bigcontrolcenter-base || exit 1
-rm PKGBUILD
-wget https://raw.githubusercontent.com/vcatafesta/bigcontrolcenter-base/main/pkgbuild/PKGBUILD
-sed -i 's|url="https://github.com/biglinux/$pkgname"|url="https://github.com/vcatafesta/$pkgname"|'g PKGBUILD
-makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm
+if mkdir -p /tmp/biglinux-webapps-install; then
+	cd /tmp/biglinux-webapps-install || exit 1
+	[[ -e PKGBUILD ]] && rm PKGBUILD
+	if wget https://raw.githubusercontent.com/vcatafesta/biglinux-webapps/main/pkgbuild/PKGBUILD; then
+		sed -i 's|url="https://github.com/biglinux/biglinux-webapps"|url="https://github.com/vcatafesta/biglinux-webapps"|'g PKGBUILD
+		makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm || exit 1
+	fi
+fi
 
 
