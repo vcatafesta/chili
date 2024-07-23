93943f1ead2d029e487be636e6d4ac502f6b9ffe
vcatafesta <vcatafesta@gmail.com>
2024-07-22 22:58:19 -0400

    seg 22 jul 2024 22:58:19 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- download_compile_install_bigpkgs.sh
+++ download_compile_install_bigpkgs.sh
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # shellcheck shell=bash disable=SC1091,SC2039,SC2166
 #
-#  download_compile_install_bigwebapps.sh
-#  Created: 2024/07/08 - 14:35
-#  Altered: 2024/07/16 - 10:32
+#  download_compile_install_bigpkgs.sh
+#  Created: 2024/07/22 - 22:33
+#  Altered: 2024/07/22 - 22:33
 #
 #  Copyright (c) 2024-2024, Vilmar Catafesta <vcatafesta@gmail.com>
 #  All rights reserved.
@@ -31,52 +31,76 @@
 ##############################################################################
 #export LANGUAGE=pt_BR
 export TEXTDOMAINDIR=/usr/share/locale
-export TEXTDOMAIN=download_compile_install_bigwebapps.sh
+export TEXTDOMAIN=download_compile_install_bigpkgs.sh
+
+# Definir a variável de controle para restaurar a formatação original
+reset=$(tput sgr0)
+
+# Definir os estilos de texto como variáveis
+bold=$(tput bold)
+underline=$(tput smul)   # Início do sublinhado
+nounderline=$(tput rmul) # Fim do sublinhado
+reverse=$(tput rev)      # Inverte as cores de fundo e texto
+
+# Definir as cores ANSI como variáveis
+black=$(tput bold)$(tput setaf 0)
+red=$(tput bold)$(tput setaf 196)
+green=$(tput bold)$(tput setaf 2)
+yellow=$(tput bold)$(tput setaf 3)
+blue=$(tput setaf 4)
+pink=$(tput setaf 5)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+gray=$(tput setaf 8)
+orange=$(tput setaf 202)
+purple=$(tput setaf 125)
+violet=$(tput setaf 61)
+light_red=$(tput setaf 9)
+light_green=$(tput setaf 10)
+light_yellow=$(tput setaf 11)
+light_blue=$(tput setaf 12)
+light_magenta=$(tput setaf 13)
+light_cyan=$(tput setaf 14)
+bright_white=$(tput setaf 15)
 
 #debug
-export PS4='[1m[38;5;196mbash[1m[32m[download_compile_install_bigwebapps.sh][35m[15](B[m '
+export PS4='${red}${0##*/}${green}[$FUNCNAME]${pink}[$LINENO]${reset}'
 #set -x
-set -e
+#set -e
 shopt -s extglob
 
 #system
-readonly APP="bash"
-readonly _VERSION_='1.0.0-20240713 - 11:26'
-readonly distro=voidlinux
-readonly DEPENDENCIES=(grep sed wget makepkg)
+declare APP="${0##*/}"
+declare _VERSION_="1.0.0-20240722"
+declare distro="$(uname -n)"
+declare pkg
+declare -a DEPENDENCIES=(grep sed wget makepkg)
+declare -a aPackages=(
+	'bigcontrolcenter-base'
+	'biglinux-webapps'
+	'biglinux-config'
+	'auto-tweaks-browser'
+	'big-store'
+)
 
-if mkdir -p /tmp/bigcontrolcenter-base-install; then
-	cd /tmp/bigcontrolcenter-base-install || exit 1
-	[[ -e PKGBUILD ]] && rm PKGBUILD
-	if wget https://raw.githubusercontent.com/vcatafesta/bigcontrolcenter-base/main/pkgbuild/PKGBUILD; then
-		sed -i 's|url="https://github.com/biglinux/$pkgname"|url="https://github.com/vcatafesta/$pkgname"|'g PKGBUILD
-		makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm || exit 1
-	fi
-fi
+function MostraErro {
+	echo "erro: ${red}$1${reset} => comando: ${cyan}'$2'${reset} => result=${yellow}$3${reset}"
+}
+#trap 'MostraErro "$APP[$FUNCNAME][$LINENO]" "$BASH_COMMAND" "$?"; exit 1' ERR
 
-if mkdir -p /tmp/biglinux-webapps-install; then
-	cd /tmp/biglinux-webapps-install || exit 1
-	[[ -e PKGBUILD ]] && rm PKGBUILD
-	if wget https://raw.githubusercontent.com/vcatafesta/biglinux-webapps/main/pkgbuild/PKGBUILD; then
-		sed -i 's|url="https://github.com/biglinux/$pkgname"|url="https://github.com/vcatafesta/$pkgname"|'g PKGBUILD
-		makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm || exit 1
+for pkg in "${aPackages[@]}"; do
+	if mkdir -p /tmp/$pkg-install; then
+		cd /tmp/$pkg-install || exit 1
+		[[ -e PKGBUILD ]] && rm PKGBUILD
+		if wget https://raw.githubusercontent.com/vcatafesta/$pkg/main/pkgbuild/PKGBUILD; then
+			sed -i 's|url="https://github.com/biglinux/$pkgname"|url="https://github.com/vcatafesta/$pkgname"|'g PKGBUILD
+			if ! makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm; then
+				mapfile -t aPacknames <<<"$(find /tmp/$pkg-install -iname "$pkg-*pkg.tar*")"
+				for pkginstall in "${aPacknames[@]}"; do
+					sudo pacman -U $pkginstall --overwrite \*
+				done
+			fi
+		fi
 	fi
-fi
-
-if mkdir -p /tmp/biglinux-config-install; then
-	cd /tmp/biglinux-config-install || exit 1
-	[[ -e PKGBUILD ]] && rm PKGBUILD
-	if wget https://raw.githubusercontent.com/vcatafesta/biglinux-config/main/pkgbuild/PKGBUILD; then
-		sed -i 's|url="https://github.com/biglinux/$pkgname"|url="https://github.com/vcatafesta/$pkgname"|'g PKGBUILD
-		makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm || exit 1
-	fi
-fi
-
-if mkdir -p /tmp/auto-tweaks-browser-install; then
-	cd /tmp/auto-tweaks-browser-install || exit 1
-	[[ -e PKGBUILD ]] && rm PKGBUILD
-	if wget https://raw.githubusercontent.com/vcatafesta/auto-tweaks-browser/main/pkgbuild/PKGBUILD; then
-		sed -i 's|url="https://github.com/biglinux/$pkgname"|url="https://github.com/vcatafesta/$pkgname"|'g PKGBUILD
-		makepkg --force --install --clean --cleanbuild --syncdeps --noconfirm || exit 1
-	fi
-fi
+done
