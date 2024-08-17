c3260dc2ee744f1b17e01d47c411228f7c7b05c2
vcatafesta <vcatafesta@gmail.com>
2024-08-17 17:21:41 -0400

    sáb 17 ago 2024 17:21:41 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- bashrc.sh
+++ bashrc.sh
@@ -1556,7 +1556,7 @@ function gpush() {
 	local reset=$(tput sgr0)
 	local mainbranch="$(getbranch)"
 
-	log_wait_msg "${red}Iniciando git push ${reset}"
+	log_wait_msg "${red}Iniciando git push in ${yellow}${mainbranch}'${reset}"
 	#export GIT_CURL_VERBOSE=1
 	git checkout "$mainbranch"
 	sudo git config --global http.postBuffer 524288000
